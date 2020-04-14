99b317170b4a3868bf1a0bf2ee0dc3e65b3f40ef
kee <kee@srees-imac.lan>
2020-04-14 13:09:00 -0400

    Refactored app.py

--- backend/app.py
+++ backend/app.py
@@ -9,25 +9,47 @@ app = Flask(__name__)
 
 # Load Cached Graphs from Memory
 print("Loading Graphs")
-infile = open("../../cached_graphs/amherst_graph_no_elevation_drive.pkl", 'rb')
+infile = open("./data/cached_graphs/amherst_graph_no_elevation_drive.pkl", 'rb')
 amherst_graph = pkl.load(infile)
 
-infile = open("../../cached_graphs/amherst_graph_elevation_drive_projected.pkl", 'rb')
+infile = open("./data/amherst_graph_elevation_drive_projected.pkl", 'rb')
 amherst_projected = pkl.load(infile)
 g_nodes = ox.graph_to_gdfs(amherst_graph, edges=False)
 
 
-# @app.route('/test', methods=['POST'])
-# def get_route():
-    # data = json.loads(request.data)
+@app.route('/route', methods=['POST'])
+def route():
+    """
+	request.data: {
+		start: "Start Addres Lane"
+		destination: "Dest"
+		goal: "Max/ Min"
+		limit: "##"
+		algorithm: "ucs/astar/bfs/..."
+	}
+	"""
+    data = json.loads(request.data)
+    start_node = ox.get_nearest_node(data.start)
+    dest_node = ox.get_nearest_node(data.destination)
+    algorithm = data.algorithm
 
-    # start_lat = data[0]["lat"]
-    # start_long = data[0]["lng"]
-    #
-    # end_lat = data[1]["lat"]
-    # end_long = data[1]["lng"]
+    return get_route(start_node, end_node, algorithm)
 
-def get_route(start_lat, start_long, end_lat, end_long):  # common out this line and uncomment the above lines to run with flask
+def get_route(start_node, dest_node, algorithm='astar',name='Route', color = (255,0,0)):
+
+    if algorithm == 'astar':
+        context = Context(strategies.StrategyAStar(amherst_graph, amherst_projected, g_nodes))
+        path = context.run_strategy_route(start_node, end_node)
+    if algorithm == 'ucs':
+        context.strategy = strategies.StrategyUCS(amherst_graph, amherst_projected, g_nodes)
+        path = context.run_strategy_route(start_node, end_node)
+
+    path = [[lng, lat] for [lat, lng] in path]
+    return { path: path, name: name, color: color }
+    
+    
+
+def get_route_(start_lat, start_long, end_lat, end_long):  # common out this line and uncomment the above lines to run with flask
     start_node = ox.get_nearest_node(amherst_graph, (start_lat, start_long))
     end_node = ox.get_nearest_node(amherst_graph, (end_lat, end_long))
     print("Set Strategy to A*.")
